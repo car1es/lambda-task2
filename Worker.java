ec2954a3d4d3b7b1d38808e126fef2c356e8472d
Alexey Gorokhov <car1es@bk.ru>
2022-10-24 13:17:19 +0300

    start() changed

--- Worker.java
+++ Worker.java
@@ -10,11 +10,11 @@ public class Worker {
     }
 
     public void start() {
-        for (int i = 0; i < 100; i++) {
-            if (i + 1 == 33) {
-                errorCallback.onError("Task " + (i + 1) + " is missed");
+        for (int i = 1; i <= 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is missed");
             } else {
-                callback.onDone("Task " + (i + 1) + " is done");
+                callback.onDone("Task " + i + " is done");
             }
 
         }
